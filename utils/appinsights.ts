0dcc50f388a22ce71838c2946a0cbea03c5d00dc
Emanuele De Cupis <ema.decu@gmail.com>
2021-04-21 13:24:46 +0200

    refactor appinsight module

--- utils/appinsights.ts
+++ utils/appinsights.ts
@@ -1,26 +1,14 @@
 import { initAppInsights } from "@pagopa/ts-commons/lib/appinsights";
-import { IntegerFromString } from "@pagopa/ts-commons/lib/numbers";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as ai from "applicationinsights";
-import { identity, toString } from "fp-ts/lib/function";
-
-// the internal function runtime has MaxTelemetryItem per second set to 20 by default
-// @see https://github.com/Azure/azure-functions-host/blob/master/src/WebJobs.Script/Config/ApplicationInsightsLoggerOptionsSetup.cs#L29
-const DEFAULT_SAMPLING_PERCENTAGE = 5;
+import { IConfig } from "./config";
 
 // Avoid to initialize Application Insights more than once
-export const initTelemetryClient = (env = process.env) =>
+export const initTelemetryClient = (
+  env: IConfig
+): ai.TelemetryClient | ReturnType<typeof initAppInsights> =>
   ai.defaultClient
     ? ai.defaultClient
-    : NonEmptyString.decode(env.APPINSIGHTS_INSTRUMENTATIONKEY)
-        .map(k =>
-          initAppInsights(k, {
-            disableAppInsights: env.APPINSIGHTS_DISABLE === "true",
-            samplingPercentage: IntegerFromString.decode(
-              env.APPINSIGHTS_SAMPLING_PERCENTAGE
-            ).getOrElse(DEFAULT_SAMPLING_PERCENTAGE)
-          })
-        )
-        .fold(l => {
-          throw new Error(`Can not init AppInsights: ${toString(l)}`);
-        }, identity);
+    : initAppInsights(env.APPINSIGHTS_INSTRUMENTATIONKEY, {
+        disableAppInsights: env.APPINSIGHTS_DISABLE === "true",
+        samplingPercentage: env.APPINSIGHTS_SAMPLING_PERCENTAGE
+      });
--- utils/config.ts
+++ utils/config.ts
@@ -5,24 +5,34 @@
  * The configuration is evaluate eagerly at the first access to the module. The module exposes convenient methods to access such value.
  */
 
+import { IntegerFromString } from "@pagopa/ts-commons/lib/numbers";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { withDefault } from "@pagopa/ts-commons/lib/types";
 import * as t from "io-ts";
 
 // global app configuration
 export type IConfig = t.TypeOf<typeof IConfig>;
-export const IConfig = t.interface({
-  COSMOSDB_KEY: NonEmptyString,
-  COSMOSDB_NAME: NonEmptyString,
-  COSMOSDB_URI: NonEmptyString,
+export const IConfig = t.intersection([
+  t.interface({
+    COSMOSDB_KEY: NonEmptyString,
+    COSMOSDB_NAME: NonEmptyString,
+    COSMOSDB_URI: NonEmptyString,
 
-  CachedStorageConnection: NonEmptyString,
-  SLOT_TASK_HUBNAME: NonEmptyString,
-  STATIC_BLOB_ASSETS_ENDPOINT: NonEmptyString,
-  STATIC_WEB_ASSETS_ENDPOINT: NonEmptyString,
+    CachedStorageConnection: NonEmptyString,
+    SLOT_TASK_HUBNAME: NonEmptyString,
+    STATIC_BLOB_ASSETS_ENDPOINT: NonEmptyString,
+    STATIC_WEB_ASSETS_ENDPOINT: NonEmptyString,
 
-  isProduction: t.boolean
-});
+    APPINSIGHTS_INSTRUMENTATIONKEY: NonEmptyString,
+    // the internal function runtime has MaxTelemetryItem per second set to 20 by default
+    // @see https://github.com/Azure/azure-functions-host/blob/master/src/WebJobs.Script/Config/ApplicationInsightsLoggerOptionsSetup.cs#L29
+    APPINSIGHTS_SAMPLING_PERCENTAGE: withDefault(IntegerFromString, 5),
+
+    isProduction: t.boolean
+  }),
+  t.partial({ APPINSIGHTS_DISABLE: t.string })
+]);
 
 // No need to re-evaluate this object for each call
 const errorOrConfig: t.Validation<IConfig> = IConfig.decode({
