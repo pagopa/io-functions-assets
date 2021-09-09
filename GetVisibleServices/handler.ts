2b541faa21d4091ed524f321f2a98d15f7f1c301
gquadrati <greta.quadrati@pagopa.it>
2021-09-09 17:37:11 +0200

    refactor GetVisibleServices

--- GetVisibleServices/handler.ts
+++ GetVisibleServices/handler.ts
@@ -1,4 +1,3 @@
-import { StrMap } from "fp-ts/lib/StrMap";
 import * as t from "io-ts";
 
 import * as express from "express";
@@ -25,39 +24,51 @@ import { wrapRequestHandler } from "@pagopa/io-functions-commons/dist/src/utils/
 import { PaginatedServiceTupleCollection } from "@pagopa/io-functions-commons/dist/generated/definitions/PaginatedServiceTupleCollection";
 import { ServiceId } from "@pagopa/io-functions-commons/dist/generated/definitions/ServiceId";
 
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
+import * as O from "fp-ts/lib/Option";
+import * as TE from "fp-ts/lib/TaskEither";
+
 type IGetVisibleServicesHandlerRet =
   | IResponseSuccessJson<PaginatedServiceTupleCollection>
   | IResponseErrorInternal;
 
 type IGetVisibleServicesHandler = () => Promise<IGetVisibleServicesHandlerRet>;
 
-/**
- * Returns all the visible services (is_visible = true).
- */
 export function GetVisibleServicesHandler(
   blobService: BlobService
 ): IGetVisibleServicesHandler {
   return async () => {
     const errorOrMaybeVisibleServicesJson = await getBlobAsObject(
-      t.dictionary(ServiceId, VisibleService),
+      t.record(ServiceId, VisibleService),
       blobService,
       VISIBLE_SERVICE_CONTAINER,
       VISIBLE_SERVICE_BLOB_ID
     );
-    return errorOrMaybeVisibleServicesJson.fold<IGetVisibleServicesHandlerRet>(
-      error =>
-        ResponseErrorInternal(
-          `Error getting visible services list: ${error.message}`
-        ),
-      maybeVisibleServicesJson => {
-        const servicesTuples = toServicesTuple(
-          new StrMap(maybeVisibleServicesJson.getOrElse({}))
-        );
-        return ResponseSuccessJson({
-          items: servicesTuples,
-          page_size: servicesTuples.length
-        });
-      }
+    return pipe(
+      errorOrMaybeVisibleServicesJson,
+      E.bimap(
+        error =>
+          ResponseErrorInternal(
+            `Error getting visible services list: ${error.message}`
+          ),
+        maybeVisibleServicesJson => {
+          const servicesTuples = pipe(
+            maybeVisibleServicesJson,
+            // tslint:disable-next-line: no-inferred-empty-object-type
+            O.getOrElse(() => ({})),
+            Object.entries,
+            _ => new Map<string, VisibleService>(_),
+            toServicesTuple
+          );
+
+          return ResponseSuccessJson({
+            items: servicesTuples,
+            page_size: servicesTuples.length
+          });
+        }
+      ),
+      E.toUnion
     );
   };
 }
